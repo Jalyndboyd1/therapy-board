bb3ea01e2e0d33136da6f71f64e01a47a42c300c
DESKTOP-39P86SN\Jalyn <JBoyd@zincpartners.us>
2023-01-06 00:54:08 -0500

    Feature: Added Login Button

--- src/containers/DisplayHeader.js
+++ src/containers/DisplayHeader.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { selectRoute } from "../features/reducers/counterSlice";
 import { useSelector } from "react-redux";
-import "./css/DisplayHeader.css"
+import "./css/DisplayHeader.css";
+import LoginIcon from "@mui/icons-material/Login";
+import { Button } from "@mui/material";
 
 function DisplayHeader() {
   const currentRoute = useSelector(selectRoute);
   return (
     <div className="displayHeader">
       <h3>{currentRoute}</h3>
+      <Button sx={{
+        color:'#a9aadd'
+      }}>
+        <LoginIcon />
+      </Button>
     </div>
   );
 }
