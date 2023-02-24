5b378e1298cd8d004a87ed2b3d2e6901952d9ab5
Jalyndboyd1 <jalyndboyd1@gmail.com>
2023-02-23 18:19:44 -0800

    Install firebase

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -7,13 +7,15 @@ export default function Sidebar() {
     <div className="sidebar">
       <header className="sidebar__header">
         <img src={logo} alt="" />
+        {/* CHange logo to: In House */}
       </header>
       {/* Sidebar Routes */}
+      {/* links will have dynamic paths */}
       <div className="sidebar__routes">
         <SidebarOption path="" label="Home" />
         <SidebarOption path="session" label="Session" />
-        <SidebarOption path="calendar" label="Calendar" />
         <SidebarOption path="goals" label="Goal Board" />
+        <SidebarOption path="calendar" label="Calendar" />
         <SidebarOption path="journal" label="Journal" />
       </div>
     </div>
